86392ec8175ffe0f93705e3b3f97c5b8bb3ca826
Elie Roux <elie.roux@telecom-bretagne.eu>
2018-01-30 14:34:42 +0100

    first version of the export

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Kangyur-scripts
 Internal scripts to play with Kangyur correspondence tables
+
+Kangyurs:
+
+- `W22084`, `O1GS12980` (D) contains IDs for `D` (~correct) and `Toh`
+- `W1PD96682`, `O1PD112371` (CK), idem
+- `W4CZ7445`, `O1PD185519` (J), idem
+- `W22703`, `O01JW005` (N) contains IDs for `D`, `H` and `N` (~correct)
+- `W26071`, `O1PD11016` (H) contains IDs for `D`, `H` (~correct) and `N`
+- `W29468`, `O1PD19510` (U) contains IDs for `D`, `H` and `N` (what would be correct? no idea, maybe `N`)
+- `W1PD96685`, `O4CZ3720` (C) contains IDs for `N`, stog, `D`, `H`
+
+
+Example of SPARQL request to produce the csv files:
+
+```
+PREFIX bdo: <http://purl.bdrc.io/ontology/core/>
+PREFIX bdr: <http://purl.bdrc.io/resource/>
+PREFIX adm: <http://purl.bdrc.io/ontology/admin/>
+
+SELECT ?nodeId ?N ?D
+WHERE {
+  GRAPH bdr:W1PD96685 {
+  	?subject adm:workLegacyNode ?nodeId .
+  	OPTIONAL { ?subject bdo:workKaTenSiglaD ?D }
+  	OPTIONAL { ?subject bdo:workKaTenSiglaH ?N }
+  	FILTER ( bound(?N) || bound(?D) )
+  }
+}
+```
---
+++ rkts-nodeid.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import csv
+import json
+
+result = {}
+
+cattorkts = {}
+
+def fillCatalogue(filename, catname):
+    global cattorkts
+    res = {}
+    cattorkts[catname] = res
+    with open(filename, newline='') as csvfile:
+        r = csv.reader(csvfile)
+        for row in r:
+            try:
+                rktsid = int(row[1])
+                catid = int(row[0])
+            except ValueError as ex:
+                pass
+            res[catid] = rktsid
+
+def fillCatalogues():
+    fillCatalogue('D-rKTs.csv', 'D')
+    fillCatalogue('H-rKTs.csv', 'H')
+    fillCatalogue('N-rKTs.csv', 'N')
+
+def fillOutline(filename, workname, catname):
+    global cattorkts, result
+    catcorr = cattorkts[catname]
+    with open(filename, newline='') as csvfile:
+        r = csv.reader(csvfile)
+        for row in r:
+            nodeid = row[0]
+            catid = 0
+            try:
+                catid = int(row[1])
+            except ValueError as ex:
+                continue
+            if catid not in catcorr:
+                continue
+            rktsid = catcorr[catid]
+            if rktsid not in result:
+                result[rktsid] = {}
+            idcorrsgen = result[rktsid]
+            if workname not in idcorrsgen:
+                idcorrsgen[workname] = []
+            idcorrsgen[workname].append(nodeid)
+
+
+def fillOutlines():
+    fillOutline('W1PD96682-D.csv', 'W1PD96682', 'D')
+    fillOutline('W1PD96685-N.csv', 'W1PD96685', 'N')
+    fillOutline('W4CZ7445-D.csv', 'W4CZ7445', 'D')
+    fillOutline('W22084-D.csv', 'W22084', 'D')
+    fillOutline('W22703-N.csv', 'W22703', 'N')
+    fillOutline('W26071-H.csv', 'W26071', 'H')
+    fillOutline('W29468-N.csv', 'W29468', 'N')
+
+fillCatalogues()
+fillOutlines()
+with open('rkts-bdrc.json', 'w') as fp:
+    json.dump(result, fp, indent=4, sort_keys=True)
